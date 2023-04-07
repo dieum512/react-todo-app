0fc855d57b15e09646eee4de139c30e0e8a52aef
dieum512 <akonkwadieum222@gmail.com>
2023-04-07 11:03:33 +0200

    Add the linters

---
+++ src/components/TodosLogic.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+// other imported components here
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+  return (
+    <div>
+    <InputTodo />
+    <TodosList todosProps={todos} setTodos={setTodos} />
+  </div>
+  );
+};
+export default TodosLogic;
